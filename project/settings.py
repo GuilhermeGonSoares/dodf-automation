e0ccdda1664443a8d1d44a0d8353a9e9676aabed
Guilherme Goncalves Soares <guilherme.soares@tc.df.gov.br>
2023-08-29 15:54:28 -0300

    add fontawesome

--- project/settings.py
+++ project/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'mpc',
     'user',
+    'fontawesomefree'
 ]
 
 MIDDLEWARE = [
@@ -140,7 +141,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     BASE_DIR/'website'
